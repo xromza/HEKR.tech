746af171374bc0ee17c5753c869a9189ad639b5a
xromza <xxxromza@gmail.com>
2026-04-07 01:53:09 +0300

    Postgres downgraded due to experimental functions error

--- backend/src/test/java/com/hekr/store/StoreApplicationTests.java
+++ backend/src/test/java/com/hekr/store/StoreApplicationTests.java
@@ -13,7 +13,7 @@ class StoreApplicationTests {
 
     @Container
     @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:18-alpine");
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
 
     @Test
     void contextLoads() {
