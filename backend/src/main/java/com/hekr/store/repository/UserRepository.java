231d341de6781ae97bc5951ccbdb9672b5e0d40c
xromza <xxxromza@gmail.com>
2026-04-21 01:55:40 +0300

    feat(repo): add new methods

--- backend/src/main/java/com/hekr/store/repository/UserRepository.java
+++ backend/src/main/java/com/hekr/store/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.hekr.store.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hekr.store.model.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    Optional<User> findByLogin(String login);
 }
