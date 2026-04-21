5af27ac52f2624df5155dedb33129374bd187066
xromza <xxxromza@gmail.com>
2026-04-21 19:20:53 +0300

    feat(auth): new endpoints add

--- backend/src/main/java/com/hekr/store/controller/AuthController.java
+++ backend/src/main/java/com/hekr/store/controller/AuthController.java
@@ -3,14 +3,17 @@ package com.hekr.store.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hekr.store.auth.AuthRefreshRequestDto;
 import com.hekr.store.auth.AuthRequestDto;
 import com.hekr.store.auth.AuthResponseDto;
 import com.hekr.store.auth.UserRegistrationDto;
+import com.hekr.store.dto.StatusDto;
 import com.hekr.store.service.AuthService;
 
 
 import lombok.RequiredArgsConstructor;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,18 +26,21 @@ public class AuthController {
     private final AuthService authService;
     
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody AuthRequestDto request) {
-        try {
+    public ResponseEntity<AuthResponseDto> login(@RequestBody AuthRequestDto request) {
         return ResponseEntity.ok(authService.authenticate(request));
-        } catch (Exception e) {
-            e.printStackTrace(); // Смотрим, что упало на самом деле
-            return ResponseEntity.status(401).body(e.getMessage());
-        }
     }
     
     @PostMapping("/register")
     public ResponseEntity<AuthResponseDto> register(@RequestBody UserRegistrationDto request) {
         return ResponseEntity.ok(authService.register(request));
     }
-    
+
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthResponseDto> refresh(@RequestBody AuthRefreshRequestDto request) {
+        return ResponseEntity.ok(authService.refreshAccessToken(request));
+    }
+    @PostMapping("/logout")
+    public ResponseEntity<StatusDto> logout(@RequestBody AuthRefreshRequestDto request) {
+        return ResponseEntity.ok(authService.logout(request));
+    }
 }
