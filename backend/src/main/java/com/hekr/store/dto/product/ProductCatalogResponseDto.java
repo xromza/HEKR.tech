3b09df6d03c15f9a81ff5df3f3a29002a103184f
xromza <xxxromza@gmail.com>
2026-04-24 04:08:35 +0300

    fix(dto): fix product catalog response dto methods

--- backend/src/main/java/com/hekr/store/dto/product/ProductCatalogResponseDto.java
+++ backend/src/main/java/com/hekr/store/dto/product/ProductCatalogResponseDto.java
@@ -6,6 +6,7 @@ import com.hekr.store.interfaces.ProductDtoInterface;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,6 +15,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ProductCatalogResponseDto implements ProductDtoInterface {
     @Schema(description = "Уникальный идентификатор продукта", example = "1")
     private Long id;
