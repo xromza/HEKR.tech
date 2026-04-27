5c1e0b89ad1735e656d0ec0450761da0f12ceb12
xromza <xxxromza@gmail.com>
2026-04-28 00:31:35 +0300

    feat(exceptions): add new error type

--- backend/src/main/java/com/hekr/store/dto/error/MapErrorResponseDto.java
+++ backend/src/main/java/com/hekr/store/dto/error/MapErrorResponseDto.java
@@ -13,7 +13,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class ErrorResponseDto {
+public class MapErrorResponseDto {
     private String error;
-    private String description;
+    private Map<String, String> errors;
 }
