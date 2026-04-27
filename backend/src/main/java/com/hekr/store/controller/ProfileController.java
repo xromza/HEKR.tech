b39fcb656b753e8dad26a20958ac393abb8d1289
xromza <xxxromza@gmail.com>
2026-04-28 00:34:14 +0300

    feat(profile): add change details

--- backend/src/main/java/com/hekr/store/controller/ProfileController.java
+++ backend/src/main/java/com/hekr/store/controller/ProfileController.java
@@ -3,14 +3,18 @@ package com.hekr.store.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hekr.store.dto.user.UserEditDto;
 import com.hekr.store.dto.user.UserResponseDto;
 import com.hekr.store.service.ProfileService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/profile")
@@ -23,4 +27,8 @@ public class ProfileController {
         return profileService.getProfileData(userDetails);
     }
 
+    @PatchMapping
+    public UserResponseDto updateAccountDetails(@AuthenticationPrincipal UserDetails userDetails, @Valid @RequestBody UserEditDto userEditDto) {
+        return profileService.updateProfileData(userDetails, userEditDto);
+    }
 }
