2274eeb7251e8f152e346e5a2564a2243b079ee0
xromza <xxxromza@gmail.com>
2026-05-07 23:54:25 +0300

    fix: header counts receive

--- frontend/app/lib/getHeader.ts
+++ frontend/app/lib/getHeader.ts
@@ -1,6 +1,6 @@
 import { HeaderInterface } from "@/types/HeaderInterface";
 export async function getHeader(): Promise<HeaderInterface> {
-  const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8080/api';
+  const API_URL = process.env.BACKEND_URL || process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8080/api';
   try {
     const res = await fetch(`${API_URL}/v1/header`, {
       next: { revalidate: 600 }
--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,10 +1,11 @@
+export const dynamic = 'force-dynamic';
+
 import { getHeader } from '@/app/lib/getHeader';
 import HeaderClient from './HeaderClient';
 
 export default async function Header() {
 
   const header = await getHeader();
-  console.log(header.manCount)
   return (
     <HeaderClient
       manCount={header.manCount}
