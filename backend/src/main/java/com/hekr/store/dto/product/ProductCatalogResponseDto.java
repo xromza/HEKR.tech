106be07edb106fcbb2e28b35ca4507f614e284ab
xromza <xxxromza@gmail.com>
2026-05-14 04:11:27 +0300

    feat(backend): cors configure & wholesaleThreshold field add

--- backend/src/main/java/com/hekr/store/dto/product/ProductCatalogResponseDto.java
+++ backend/src/main/java/com/hekr/store/dto/product/ProductCatalogResponseDto.java
@@ -19,28 +19,29 @@ import lombok.Setter;
 public class ProductCatalogResponseDto implements ProductDtoInterface {
     @Schema(description = "Уникальный идентификатор продукта", example = "1")
     private Long id;
-    
+
     private String brand;
 
     @Schema(description = "Название товара", example = "Смартфон Samsung Galaxy A51")
     private String title;
-    
+
     @Schema(description = "ID категории товара", example = "5")
     private Long categoryId;
-    
+
     @Schema(description = "Название категории товара", example = "Электроника")
     private String categoryName;
-    
+
     @Schema(description = "Статус активности товара", example = "true")
     private Boolean isActive;
-    
+
     @Schema(description = "Оптовая цена товара", example = "15000.00")
     private BigDecimal priceWholesale;
-    
+
     @Schema(description = "Розничная цена товара", example = "19999.99")
     private BigDecimal priceRetail;
-    
 
+    private Integer wholesaleThreshold;
+    
     @Schema(description = "URL основного изображения товара", example = "https://example.com/images/product-1.jpg")
     private String mainImageUrl;
 }
